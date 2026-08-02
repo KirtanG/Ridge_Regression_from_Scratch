1f8fd8f2c3eb55bce7c3b5834bf2ea74ef99f8cb
KirtanG <kirtangoswami97@gmail.com>
2026-08-02 20:02:57 +0530

    Created source file for Ridge Regression using Gradient Descent

---
+++ source/Ridge_Regression_GD.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+class RidgeRegressionGD:
+    """
+    Implements Multiple Ridge Regression using NumPy.
+
+    This class provides methods to fit a multiple ridge regression model
+    to training data and make predictions on new data. It calculates the
+    coefficients and intercept using Gradient descent.
+    """
+    def __init__(self,alpha = 0.1,learning_rate = 0.001,epochs = 500):
+        self.intercept_ = None
+        self.coef_ = None 
+        self.epochs = epochs
+        self.alpha = alpha
+        self.learning_rate = learning_rate
+        
+    def fit(self,X_train: np.ndarray, y_train:np.ndarray) -> None:
+        """
+         Attributes:
+            coef_ (np.ndarray): The learned coefficients for the features.
+            intercept_ (float): The learned intercept (bias) term.
+        """
+        # Handle 1D input (single feature case)
+        if X_train.ndim == 1:
+            X_train = X_train.reshape(-1, 1)
+            
+        # Initialise all the W to 1
+        self.coef_ = np.ones(X_train.shape[1])
+        self.intercept_ = 0
+        
+        # create the W matrix aliased as theta 
+        theta = np.insert(self.coef_,0,self.intercept_)
+        
+        X_train = np.insert(X_train,0,1,axis=1)
+        # Add Gradient Scaling
+        m = X_train.shape[0]
+        
+        for i in range(self.epochs):
+            theta_der = (1/m) * (np.dot(X_train.T,X_train).dot(theta) - np.dot(X_train.T,y_train) + self.alpha * np.insert(theta[1:],0,0))
+            theta = theta - self.learning_rate * theta_der
+            
+        self.intercept_ = theta[0]
+        self.coef_ = theta[1:]
+
+    def predict(self,X_test: np.ndarray) -> np.ndarray:
+        """
+         Args:
+            X_train (np.ndarray): Training feature data. Can be 1D or 2D.
+                                  If 1D, it will be reshaped to 2D.
+            y_train (np.ndarray): Training target values.
+        """
+        # Handle 1D input
+        is_1D = False
+        if X_test.ndim == 1:
+            X_test = X_test.reshape(-1, 1)
+            is_1D = True
+        
+        # Handle 1D coefficients
+        if self.coef_.ndim == 1:
+            self.coef_ = self.coef_.reshape(-1, 1)
+        
+        y_pred = np.dot(X_test, self.coef_) + self.intercept_
+        
+        # Flatten if input was 1D
+        if is_1D:
+            y_pred = y_pred.flatten()
+        
+        return y_pred
+        
+        
